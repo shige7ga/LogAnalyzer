f6aebb182e6baeaefbb51519c6113c6c2d7ecede
shige7ga <ameft_80@yahoo.co.jp>
2026-01-09 15:21:16 +0900

    Bulk InsertでのInsertファイル作成及びSQLをPDOに編集

--- src/dbOperation.php
+++ src/dbOperation.php
@@ -2,8 +2,8 @@
 
 require __DIR__ . '/vendor/autoload.php';
 
-// データベースに接続する関数
-function dbConnect()
+// PDO接続を確立する関数
+function connectPdo()
 {
     // .envファイルの読み込み(環境変数を設定)
     $dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
@@ -14,89 +14,73 @@ function dbConnect()
     $dbPassword = $_ENV['DB_PASSWORD'];
     $dbDatabase = $_ENV['DB_DATABASE'];
 
-    $link = mysqli_connect($dbHost, $dbUsername, $dbPassword, $dbDatabase);
-    if (!$link) {
-        echo 'データベース接続失敗' . PHP_EOL;
-        echo 'debugging error：' . mysqli_connect_error() . PHP_EOL;
+    $dsn = "mysql:host=$dbHost;dbname=$dbDatabase;charset=utf8mb4";
+    try {
+        $pdo = new PDO(
+            $dsn,
+            $dbUsername,
+            $dbPassword,
+            [
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::MYSQL_ATTR_LOCAL_INFILE => true,
+            ]
+        );
+    } catch (PDOException $error) {
+        error_log($error->getMessage());
+        echo 'DB接続失敗' . PHP_EOL;
         exit;
     }
-    return $link;
+    return $pdo;
 }
 
 // テーブルを削除する関数
-function dropTable($link)
+function dropTable($pdo)
 {
-    $dropTableSql = 'DROP TABLE IF EXISTS logs;';
-    $result = mysqli_query($link, $dropTableSql);
-    if ($result) {
+    try {
+        $sql = "DROP TABLE IF EXISTS logs";
+        $pdo->exec($sql);
         echo "テーブルを削除しました" . PHP_EOL;
-    } else {
-        echo "テーブル削除に失敗しました" . PHP_EOL;
-        echo 'debugging error：' . mysqli_error($link) . PHP_EOL;
-        exit;
+    } catch (PDOException $error) {
+        echo 'エラー発生：'. $error->getMessage() . PHP_EOL;
     }
 }
 
 // テーブルを作成する関数
-function createTable($link)
+function createTable($pdo)
 {
-    $createTableSql = <<<EOT
-CREATE TABLE logs (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  domain_code VARCHAR(255) NOT NULL,
-  page_title VARCHAR(255) NOT NULL,
-  count_views INT NOT NULL,
-  total_response_size BIGINT NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-EOT;
-    $result = mysqli_query($link, $createTableSql);
-    if ($result) {
-        echo "テーブルを作成しました" . PHP_EOL;
-    } else {
-        echo "テーブル作成に失敗しました" . PHP_EOL;
-        echo 'debugging error：' . mysqli_error($link) . PHP_EOL;
-        exit;
-    }
-}
+    $sql = <<<EOT
+        CREATE TABLE logs (
+            id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+            domain_code VARCHAR(255) NOT NULL,
+            page_title VARCHAR(255) NOT NULL,
+            count_views INT NOT NULL,
+            total_response_size BIGINT NOT NULL
+        ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    EOT;
 
-// ファイルからデータを取得し、INSERT文を生成する関数
-function getInsertSentence($link, string $filePath): string
-{
-    $handle = fopen($filePath, 'r');
-    if ($handle === false) {
-        throw new RuntimeException('ファイルを開けませんでした') . PHP_EOL;
+    try {
+        $pdo->exec($sql);
+        echo "テーブルを作成しました" . PHP_EOL;
+    } catch (PDOException $error) {
+        echo 'エラー発生：'. $error->getMessage() . PHP_EOL;
     }
-    $insertCount = 0;
-    while (($line = fgets($handle)) !== false) {
-        $data = explode(' ', $line);
-        if ($data[0] === '') {
-            $domainCord = '(' . "''" . ", '";
-        } else {
-            $domainCord = "('" . $data[0] . "', '";
-        }
-        $insertValues = $domainCord . $data[1] . "', " . $data[2] . ', ' . trim($data[3]) . ')';
-        $insertSentence = <<<EOT
-            INSERT INTO logs (
-                domain_code,
-                page_title,
-                count_views,
-                total_response_size
-                ) VALUES {$insertValues};
-        EOT;
-        insertData($link, $insertSentence);
-        $insertCount++;
-    }
-    fclose($handle);
-    return strval($insertCount) . '件のデータを挿入しました' . PHP_EOL;
 }
 
 // データを挿入する関数
-function insertData($link, string $insertSentence)
+function insertData($pdo)
 {
-    $result = mysqli_query($link, $insertSentence);
-    if (!$result) {
-        echo "データの挿入に失敗しました" . PHP_EOL;
-        echo 'debugging error：' . mysqli_error($link) . PHP_EOL;
-        exit;
+    $sql = <<<EOT
+        LOAD DATA INFILE '/var/lib/mysql-files/rawData.txt'
+        INTO TABLE logs
+        FIELDS TERMINATED BY ' '
+        LINES TERMINATED BY '\n'
+        (domain_code, page_title, count_views, total_response_size)
+    EOT;
+
+    try {
+        $insertCount = $pdo->exec($sql);
+        echo "{$insertCount}件のデータを挿入しました" . PHP_EOL;
+    } catch (PDOException $error) {
+        echo 'エラー発生：'. $error->getMessage() . PHP_EOL;
     }
 }
--- src/insertData.php
+++ src/insertData.php
@@ -1,7 +1,5 @@
 <?php
-
 require_once 'dbOperation.php';
 
-$link = dbConnect();
-echo getInsertSentence($link, 'test.txt');
-mysqli_close($link);
+$pdo = connectPdo();
+insertData($pdo);
